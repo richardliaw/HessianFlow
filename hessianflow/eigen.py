8379c50061e34087f35880409b7fbe9434fc7a12
Richard Liaw <rliaw@berkeley.edu>
2019-08-28 12:09:55 -0700

    setup

--- hessianflow/eigen.py
+++ hessianflow/eigen.py
@@ -8,7 +8,7 @@ from .utils import *
 
 def get_eigen(model, inputs, targets, criterion, cuda = True, maxIter = 50, tol = 1e-3):
     """
-    compute the top eigenvalues of model parameters and 
+    compute the top eigenvalues of model parameters and
     the corresponding eigenvectors.
     """
     if cuda:
@@ -44,10 +44,11 @@ def get_eigen(model, inputs, targets, criterion, cuda = True, maxIter = 50, tol 
                 eigenvalue = eigenvalue_tmp
     return eigenvalue, v
 
+
 def get_eigen_full_dataset(model, dataloader, criterion, cuda = True, maxIter = 50, tol = 1e-3):
     """
-    compute the top eigenvalues of model parameters and 
-    the corresponding eigenvectors with a full dataset. 
+    compute the top eigenvalues of model parameters and
+    the corresponding eigenvectors with a full dataset.
     Notice, this is very expensive.
     """
     if cuda:
@@ -58,7 +59,7 @@ def get_eigen_full_dataset(model, dataloader, criterion, cuda = True, maxIter = 
     # If you call this functino during training, remember to change the mode back to training mode.
     model.eval()
 
-    
+
     params,_ = get_params_grad(model)
     v = [torch.randn(p.size()).to(device) for p in params]
     v = normalization(v)
@@ -70,13 +71,13 @@ def get_eigen_full_dataset(model, dataloader, criterion, cuda = True, maxIter = 
         THv = [torch.zeros(p.size()).to(device) for p in params]
         counter = 0
         for inputs, targets in dataloader:
-            
+
             if batch_size == None:
                 batch_size = targets.size(0)
-               
+
             if targets.size(0) < batch_size:
                 continue
-            
+
             model.zero_grad()
             outputs = model(inputs.to(device))
             loss = criterion(outputs, targets.to(device))
@@ -90,7 +91,7 @@ def get_eigen_full_dataset(model, dataloader, criterion, cuda = True, maxIter = 
 
         eigenvalue_tmp =group_product(THv,v).cpu().item() / float(counter)
         v = normalization(THv)
-        
+
         if eigenvalue == None:
             eigenvalue = eigenvalue_tmp
         else:
@@ -99,4 +100,67 @@ def get_eigen_full_dataset(model, dataloader, criterion, cuda = True, maxIter = 
             else:
                 eigenvalue = eigenvalue_tmp
 
+    return eigenvalue, v
+
+​
+def get_eigen_full_dataset_allreduce(model, dataloader, criterion, cuda = True, maxIter = 50, tol = 1e-3):
+    """
+    compute the top eigenvalues of model parameters and
+    the corresponding eigenvectors with a full dataset.
+    Notice, this is very expensive.
+    """
+    if cuda:
+        device = 'cuda'
+    else:
+        device = 'cpu'
+    # change the model to evaluation mode, otherwise the batch Normalization Layer will change.
+    # If you call this functino during training, remember to change the mode back to training mode.
+    model.eval()
+​
+
+    params,_ = get_params_grad(model)
+    v = [torch.randn(p.size()).to(device) for p in params]
+    for v1 in v:
+        dist.all_reduce(v1)
+    v = normalization(v)
+​
+    batch_size = None
+    eigenvalue = None
+​
+    for i in range(maxIter):
+        THv = [torch.zeros(p.size()).to(device) for p in params]
+        counter = 0
+        for inputs, targets in dataloader:
+
+            if batch_size == None:
+                batch_size = targets.size(0)
+
+            if targets.size(0) < batch_size:
+                continue
+
+            model.zero_grad()
+            outputs = model(inputs.to(device))
+            loss = criterion(outputs, targets.to(device))
+            loss.backward(create_graph=True)
+​
+            params, gradsH = get_params_grad(model)
+            Hv = torch.autograd.grad(gradsH, params, grad_outputs = v, only_inputs = True, retain_graph = False)
+​
+            THv = [THv1 + Hv1 + 0. for THv1, Hv1 in zip(THv, Hv)]
+            counter += 1
+​
+        for THv1 in THv:
+            dist.all_reduce(THv1)
+
+        eigenvalue_tmp =group_product(THv,v).cpu().item() / float(counter)
+        v = normalization(THv)
+
+        if eigenvalue == None:
+            eigenvalue = eigenvalue_tmp
+        else:
+            if abs(eigenvalue-eigenvalue_tmp)/abs(eigenvalue) < tol:
+                return eigenvalue_tmp, v
+            else:
+                eigenvalue = eigenvalue_tmp
+​
     return eigenvalue, v
---
+++ setup.py
@@ -0,0 +1,2 @@
+from setuptools import setup
+setup(name="hessianflow", version="0.1", packages=["hessianflow"])
